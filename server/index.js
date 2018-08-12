dee37477d0455b8ad9254beec2790ecf37edb983
valentincognito <valentin.vannay@gmail.com>
2018-08-12 10:53:21 +0000

    Add basic commands

--- server/index.js
+++ server/index.js
@@ -7,11 +7,11 @@ const http = require('http').Server(app)
 const io = require('socket.io')(http)
 
 //global constant
-const server_port = 3090
+const port = 3090
 
 //server init
-http.listen(server_port, "0.0.0.0", function(){
-  console.log('listening on :', server_port)
+http.listen(port, "0.0.0.0", function(){
+  console.log('listening on :', port)
 })
 
 //config
@@ -22,36 +22,29 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 })
 
-
+//socket
 io.on('connection', function (socket) {
    socket.on('new-command', function (data) {
-     console.log(data);
-     port.write(data + '\n');
+     console.log('new command:',data)
+     arduino.write(data + '\n')
    })
 })
 
 
-var port = new SerialPort('COM3', {
+var arduino = new SerialPort('/dev/ttyACM0', {
   autoOpen: false
 })
 
-port.pipe(parser);
-port.on('data', logReceivedData);
+arduino.pipe(parser)
+arduino.on('data', logReceivedData)
 
 function logReceivedData(data){
-  //data = data.toString('hex');
-  console.log('received: ' + data);
+  console.log('received: ' + data)
 }
 
 
-port.open(function (err) {
+arduino.open(function (err) {
   if (err) {
-    return console.log('Error opening port: ', err.message);
+    return console.log('Error opening port: ', err.message)
   }
-
-  // setInterval(function () {
-  //   console.log('data sent !');
-  //   data = "1";
-  //   port.write(data + '\n');
-  // }, 1000);
-});
+})
